c874cf8a51f7188c103ecdb811b42c58c5248430
Baenas <57912626+Baenas@users.noreply.github.com>
2020-01-20 20:01:20 +0100

    Just started

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,9 +1,16 @@
 class MemoryGame {
   constructor(cards){
-    this.cards = cards;
+
+
+this.pickedCards  = [];
+
+this.pairsClicked = 0;
+this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
+  shuffleCards() {
+cards.push(this.cards)
+  }
   checkIfPair(card1, card2) {}
   isFinished() {}
 }
